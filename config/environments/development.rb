6060704da22f0d11ea6e6482b14f287744c4517c
Biko Tushinde <biko@smashingboxes.com>
2013-04-19 22:39:35 -0400

    added logging from unicorn to dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,11 +1,3 @@
-Hackerschool::Application.configure do
-  # ...
-  
-end
-
-
-
-
 Laserbeard::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
